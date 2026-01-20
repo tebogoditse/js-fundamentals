2fe49ef7d0131859da581072a1f3b422a5c82719
tebogoditse <tebogoditse2@gmail.com>
2026-01-20 18:48:13 +0200

    update 5_reverse_string file

--- Day0/5_reverse_string.js
+++ Day0/5_reverse_string.js
@@ -1,13 +1,3 @@
 let word = "Hello";
-let splitWord = word.split('');
 
-let newWord = [];
-let i = 0;
-
-for (; i < splitWord.length; i++) {
-    newWord.unshift(splitWord[i]);
-}
-
-let finalWord = newWord.join('');
-
-console.log(finalWord);
+let newWord = word.reverse();
