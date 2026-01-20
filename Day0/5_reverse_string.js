d1aca846ba734093cd50aae019889e71e0cd33fb
tebogoditse <tebogoditse2@gmail.com>
2026-01-20 19:00:07 +0200

    5_reverse_string.js

--- Day0/5_reverse_string.js
+++ Day0/5_reverse_string.js
@@ -1,3 +1,10 @@
-let word = "Hello";
+let word = "JavaScript";
+let newWord = "";
 
-let newWord = word.reverse();
+let i = word.length - 1;
+for (; i >= 0; i--)
+{
+    newWord = newWord + word[i];
+}
+
+console.log(newWord);
